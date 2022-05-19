b628ca61b182880aca879c808ae2c9e298355bae
worzell <jakobfred1999@gmail.com>
2022-05-19 19:44:56 +1200

    Added routes to app.py, added templates to match routes

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-# App Routes
-
 # Home page
 @app.route('/')
 def home_page():
@@ -28,18 +26,33 @@ def classroom_main_page(class_id):
 
 # Classroom Assignments - Displays all assignments
 @app.route('/classroom/<class_id>/assignments')
-def classroom_assignments(class_id):
-    return render_template('classroom_assignments.html')
+def classroom_assignments_list(class_id):
+    return render_template('classroom_assignments_list.html')
 
-# Assignment Page - You are taken here after clicking on an assignment in the assignment list
-@app.route('/classroom/<class_id>/<assignment_id>')
-def assignment_page(class_id, assignment_id):
-    return render_template('assignment_page.html')
+# Assignment Page - View the details of a specific assignment
+@app.route('/classroom/<class_id>/assignments/<assignment_id>')
+def classroom_assignment_details(class_id, assignment_id):
+    return render_template('classroom_assignment_details.html')
+
+# Assignment Begin - This is when the student has started the assignment.
+@app.route('/classroom/<class_id>/assignments/<assignment_id>/<page_num>')
+def classroom_assignment_content(class_id, assignment_id, page_num):
+    return render_template('classroom_assignment_content.html')
 
 # User Profile - Display the users information here
 @app.route('/profile/<user>')
 def user_profile(user):
-    return render_template('user-profile.html', name=user)
+    return render_template('user_profile.html', name=user)
+
+# Student Grades - View the average grades of all their courses so far
+@app.route('/profile/<user>/grades')
+def student_grades(user):
+    return render_template('student_grades.html', name=user)
+
+# Student Drive - View all their saved notes or files
+@app.route('/profile/<user>/drive')
+def user_drive(user):
+    return render_template('user_drive.html', name=user)
 
 # Admin Page - Some pages aren't accessible unless through admin privleges yet
 @app.route('/admin')
@@ -47,5 +60,4 @@ def admin_page():
     return render_template('admin_page.html')
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(debug = True)
+    app.run(development = True)
