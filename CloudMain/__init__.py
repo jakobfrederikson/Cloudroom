129dff263a9046648809d54c4325d4d9125d0f2d
Jakob <jakobfred1999@gmail.com>
2022-05-26 16:35:34 +1200

    Code clean up

--- CloudMain/__init__.py
+++ CloudMain/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.types import PickleType
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
