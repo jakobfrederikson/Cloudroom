1b189de3b015a02acc2a29ab24600cdd92cd5c5e
270115050 <79881479+270115050@users.noreply.github.com>
2022-05-26 16:16:36 +1200

    added profile page and edit profile

--- CloudMain/forms.py
+++ CloudMain/forms.py
@@ -25,6 +25,20 @@ class LoginForm(FlaskForm):
     password = PasswordField(label="Password:", validators=[DataRequired()])
     submit = SubmitField(label='Sign in')
 
+class UpdateProfileInfo(FlaskForm):
+    first_name = StringField(label="First name", validators=[Length(min=2,max=30), DataRequired()])
+    last_name = StringField(label="Last name", validators=[Length(min=2,max=30), DataRequired()])
+    gender = SelectField(label="Select your Gender", choices=[('Male'), ('Female'), ('Other')])
+    school = SelectField(label="Select your School",
+                         choices=[('Yoobee College'), ('Victoria University'), ('Massey University')])
+    nickname = StringField(label="Nickname", validators=[Length(min=2,max=30), DataRequired()])
+    # password_hash = PasswordField(label="Password", validators=[Length(min=8), DataRequired()])
+    # verify_password = PasswordField(label="Confirm Password",validators=[EqualTo('password_hash'),DataRequired()])
+    profile_pic = RadioField(label="Profile Picture",
+        choices=[('images/profile1.jpg','Ghost'),('images/profile2.jpg','Zombie'),
+        ('images/profile3.jpg','Squid Game'),('images/profile4.jpg','Astro Cat')],validators=[DataRequired()])
+    submit = SubmitField(label='Save Changes')
+
 # Create_Classroom - by Jakob
 # This is the form used by a teacher/admin to create a classroom. The subject and room_number are not mandatory options for now.
 class Create_Classroom(FlaskForm):
