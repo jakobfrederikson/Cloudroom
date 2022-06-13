e6ab031f8494a51bd4f4a68f1a55c09b7267a613
Jakob Frederikson <270029667@yoobeestudent.ac.nz>
2022-06-13 13:39:17 +1200

    Added light check to assignment weight to see if it's above 100

--- CloudMain/static/JS/create_assignment_script.js
+++ CloudMain/static/JS/create_assignment_script.js
@@ -16,3 +16,13 @@ let max_yyyy = parseInt(yyyy) + 1;
 max = max_yyyy + '-12-31';
 dueDate.min = today;
 dueDate.max = max;
+
+// Set max integer value for the assignment weight
+let weight = document.getElementById("weight");
+
+weight.addEventListener("keydown", function() {
+    if (parseInt(weight.value) > 100)
+    {
+        alert("The value of a paper cannot be above 100.");
+    }
+});
