c596d98f874849532197f3c8daa10295b9dc52e6
Jakob <jakobfred1999@gmail.com>
2022-06-14 16:00:01 +1200

    fix layout on assignments page, change placeholder text on create question to a normal text box

--- CloudMain/forms.py
+++ CloudMain/forms.py
@@ -78,7 +78,7 @@ class Create_Assignment_Questions(FlaskForm):
     title = StringField('Title')
     type = SelectField(u'Question Type', choices=[('code', 'Python'), ('text', 'Plain Text')], validators=[DataRequired()])
     description = CKEditorField('Question Description')
-    placeholder_text = CKEditorField('Placeholder Content')
+    placeholder_text = TextAreaField('Placeholder Content')
     submit = SubmitField('Submit')
 
 
